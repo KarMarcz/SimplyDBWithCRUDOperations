094f24fd256837ee89d6884486a8538a43338aab
Karol Marczyk <KarMarcz@gmail.com>
2019-03-23 18:54:30 +0100

    Books - add html layout, controller for URL adress and what its return

---
+++ src/main/java/karol/simply/simply/controllers/BookController.java
@@ -0,0 +1,49 @@
+package karol.simply.simply.controllers;
+
+//import karol.simply.simply.repositories.BookRepository;
+////import org.springframework.stereotype.Controller;
+////import org.springframework.ui.Model;
+////import org.springframework.web.bind.annotation.RequestMapping;
+////
+////@Controller
+////public class BookController {
+////
+////    private BookRepository bookRepository;
+////
+////    public BookController(BookRepository bookRepository) {
+////        this.bookRepository = bookRepository;
+////    }
+////
+////    @RequestMapping("/books")
+////    public String getBooks(Model model){
+////
+////        model.addAttribute("books", bookRepository.findAll());
+////
+////        return "books";
+////    }
+////}
+import karol.simply.simply.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 5/18/17.
+ */
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
